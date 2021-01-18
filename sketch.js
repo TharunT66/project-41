34bd94f1734e3693025cb35b235c1143adb387b4
TharunT66 <63792253+TharunT66@users.noreply.github.com>
2021-01-18 22:26:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var man_image;
+var ground;
+var ground_options;
+var maxDrops = 100;
+var drops = [];
+var umbrella;
+var thunder, thunderCreatedFrame = 0;
+
+function preload(){
+  man_image = loadImage("man.png");
+}
+
+function setup(){
+  var canvas = createCanvas(500,500);
+
+  engine = Engine.create();
+  world  = engine.world;
+
+  
+  umbrella = new Umbrella();
+
+  if(frameCount%100===0){
+    for(var i=0; i<maxDrops;i++){
+      drops.push(new Drop(random(0,400),random(0,400),3,10));
+     }
+    }
+
+}
+
+function draw(){
+  background(0);
+  Engine.update(engine);
+
+  umbrella.display();
+  
+  for(var i=0; i<maxDrops;i++){
+    drops[i].display();
+    drops[i].update();
+  }
+ 
+  
+  drawSprites();
+}
+
+
